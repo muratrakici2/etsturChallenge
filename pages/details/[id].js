5e4aec93877c69d803123e3a753b8a0a39e9b3f5
Murat Rakıcı <muratrakici2@gmail.com>
2022-06-21 01:38:25 +0300

    fixed detail page

--- pages/details/[id].js
+++ pages/details/[id].js
@@ -2,20 +2,8 @@ const Detail = ({ data }) => {
   console.log(data);
   return <div>Detail</div>;
 };
-export async function getStaticPaths() {
-  const res = await fetch(`http://localhost:3000/api/events`);
-  const data = await res.json();
-  const paths = data.map((event) => {
-    return {
-      params: { id: event.id.toString() },
-    };
-  });
-  return {
-    paths,
-    fallback: false,
-  };
-}
-export async function getStaticProps(context) {
+
+export async function getServerSideProps(context) {
   const res = await fetch(
     `http://localhost:3000/api/events/${context.params.id}`
   );
